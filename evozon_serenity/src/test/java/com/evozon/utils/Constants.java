6561cfe7a93f70c1f9137f68502f1fd1106d91cb
EricRolandSzucs <ericrolandszucs@gmail.com>
2023-08-07 14:25:24 +0300

    properties add

--- evozon_serenity/src/test/java/com/evozon/utils/Constants.java
+++ evozon_serenity/src/test/java/com/evozon/utils/Constants.java
@@ -3,7 +3,17 @@ package com.evozon.utils;
 public class Constants {
 
     public static final String BASE_URL = "http://qa1magento.dev.evozon.com/";
-    public static final String USER_EMAIL = "cosmin@evo.com";
-    public static final String USER_PASS = "123123";
-    public static final String USER_NAME = "aaa dasda";
+    public static final String ADMIN_URL = "http://qa1magento.dev.evozon.com/admin";
+
+    public static final String USER_EMAIL = "test2@gmail.com";
+    public static final String USER_PASSWORD = "parola123";
+    public static final String USER_NAME = "Testor TT Testing";
+    public static final String ADMIN_USERNAME = "testuser";
+    public static final String ADMIN_PASSWORD = "password123";
+
+    public static final String SIMPLE_PRODUCT = "Cotton Socks";
+    public static final String SIMPLE_PRODUCT_ID = "977";
+
+    public static final String CONFIGURABLE_PRODUCT = "Flip Flops";
+    public static final String CONFIGURABLE_PRODUCT_ID = "980";
 }
